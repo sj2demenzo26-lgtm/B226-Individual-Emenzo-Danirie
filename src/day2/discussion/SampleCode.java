fe92797237ba45f16faa95d3d5c54e890fe88be0
Your Name <you@example.com>
2026-06-20 18:10:51 +0800

    Initial commit for B226

---
+++ src/day2/discussion/SampleCode.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day2.discussion;
+
+/**
+ *
+ * @author Danirie
+ */
+public class SampleCode {
+    
+    public static void main (String[] args){
+        int age = 23;
+        System.out.println(age);
+    }
+    
+}
+  
