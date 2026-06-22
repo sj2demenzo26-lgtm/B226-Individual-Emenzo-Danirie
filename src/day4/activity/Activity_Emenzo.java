4a262c3c5946a6617a5c1265d85f3e188fa8c745
Your Name <you@example.com>
2026-06-22 17:18:36 +0800

    Day4 Activity

---
+++ src/day4/activity/Activity_Emenzo.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day4.activity;
+
+public class Activity_Emenzo {
+    
+    public static void main(String args []){
+        
+        boolean isActive = true;
+        char letter = 'A';
+        byte num = 50;
+        short num2 = 200;
+        int num3 = 10_000_000;
+        long num4 = 100_000_000_000l;
+        float temp = 35.50f;
+        double average = 98.01230429;
+        String word = "Java";
+        
+        System.out.println("Is active?: " + isActive);
+        System.out.println("Grade: " + letter);
+        System.out.println("Age: " + num);
+        System.out.println("Year: " + num2);
+        System.out.println("Total: " + num3);
+        System.out.println("Balance: " + num4);
+        System.out.println("Temperature: " + temp);
+        System.out.println("Average: " + average);
+        System.out.println("Programming language: " + word);
+        
+        
+    }
+}
---
+++ src/day4/discussion/Variable.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day4.discussion;
+
+/**
+ *
+ * @author Danirie
+ */
+public class Variable {
+    
+    public static void main(String [] args){
+        String timba = "Tubig";  
+        
+        timba = "Bigas";
+        
+        
+        
+        int age = 26, age2 = 20, age3 = 22;
+        
+        double temp;
+        temp = 39.5;
+        
+        String Firstname, Middleinitial, LastName;
+        
+        Firstname = "Danirie";
+        Middleinitial = "L.";
+        LastName = "Emenzo";
+        
+        System.out.println(Firstname +" "+ Middleinitial +" "+ LastName);
+        
+    }
+    
+}
